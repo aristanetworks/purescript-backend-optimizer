8b3730f3afa42cbd1036a3ffd9eab082f7b1cd92
Nathan Faubion <nathan@awakesecurity.com>
2022-08-26 11:27:01 -0700

    Fix mutual TCO call arguments

--- backend-es/test/snapshots-out/Snapshot.Tco04.js
+++ backend-es/test/snapshots-out/Snapshot.Tco04.js
@@ -1,32 +1,32 @@
 import * as $runtime from "../runtime.js";
-const test2test1 = test2test1$b$copy => test2test1$a0$copy => test2test1$a1$copy => {
-  let test2test1$b = test2test1$b$copy, test2test1$a0 = test2test1$a0$copy, test2test1$a1 = test2test1$a1$copy, test2test1$c = true, test2test1$r;
-  while (test2test1$c) {
-    if (test2test1$b === 0) {
-      const m = test2test1$a0;
+const $test2test1 = ($test2test1$b$copy, $test2test1$a0$copy) => {
+  let $test2test1$b = $test2test1$b$copy, $test2test1$a0 = $test2test1$a0$copy, $test2test1$c = true, $test2test1$r;
+  while ($test2test1$c) {
+    if ($test2test1$b === 0) {
+      const m = $test2test1$a0;
       if (m === 2) {
-        test2test1$c = false;
-        test2test1$r = m;
+        $test2test1$c = false;
+        $test2test1$r = m;
         continue;
       }
-      test2test1$b = 1;
-      test2test1$a0 = m - 2 | 0;
+      $test2test1$b = 1;
+      $test2test1$a0 = m - 2 | 0;
       continue;
     }
-    if (test2test1$b === 1) {
-      const n = test2test1$a0;
+    if ($test2test1$b === 1) {
+      const n = $test2test1$a0;
       if (n === 1) {
-        test2test1$c = false;
-        test2test1$r = n;
+        $test2test1$c = false;
+        $test2test1$r = n;
         continue;
       }
-      test2test1$b = 0;
-      test2test1$a0 = n - 1 | 0;
+      $test2test1$b = 0;
+      $test2test1$a0 = n - 1 | 0;
       continue;
     }
   };
-  return test2test1$r;
+  return $test2test1$r;
 };
-const test2 = /* #__PURE__ */ test2test1(0);
-const test1 = /* #__PURE__ */ test2test1(1);
+const test2 = m => $test2test1(0, m);
+const test1 = n => $test2test1(1, n);
 export {test1, test2};
