a95908c668aa33dc83fa4a910e000802acd842c5
Jordan Martinez <jordan.martinez@arista.com>
2022-09-12 13:43:35 -0500

    Temporarily exclude other WriterT snapshots

--- backend-es/test/snapshots/Snapshot.TransformerWriterT01.purs
+++ backend-es/test/snapshots/Snapshot.TransformerWriterT01.purs
@@ -18,26 +18,26 @@ test1 = map fst $ runWriterT do
   tell "nothing"
   pure $ 4 + i1 + i2 + i3
 
-test2 :: forall m. MonadTell String m => MonadEffect m => m Int
-test2 = do
-  liftEffect $ Console.log "foo"
-  i1 <- liftEffect $ Random.randomInt 1 10
-  i2 <- map (_ + 1) $ liftEffect $ Random.randomInt 1 10
-  i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
-  tell "nothing"
-  pure $ 4 + i1 + i2 + i3
+-- test2 :: forall m. MonadTell String m => MonadEffect m => m Int
+-- test2 = do
+--   liftEffect $ Console.log "foo"
+--   i1 <- liftEffect $ Random.randomInt 1 10
+--   i2 <- map (_ + 1) $ liftEffect $ Random.randomInt 1 10
+--   i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
+--   tell "nothing"
+--   pure $ 4 + i1 + i2 + i3
 
-test3 :: Effect Int
-test3 = map fst $ runWriterT test2
+-- test3 :: Effect Int
+-- test3 = map fst $ runWriterT test2
 
-test4 :: forall m. MonadEffect m => m (Tuple Int String)
-test4 = runWriterT do
-  liftEffect $ Console.log "foo"
-  i1 <- liftEffect $ Random.randomInt 1 10
-  i2 <- map (_ + 1) $ liftEffect $ Random.randomInt 1 10
-  i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
-  tell "nothing"
-  pure $ 4 + i1 + i2 + i3
+-- test4 :: forall m. MonadEffect m => m (Tuple Int String)
+-- test4 = runWriterT do
+--   liftEffect $ Console.log "foo"
+--   i1 <- liftEffect $ Random.randomInt 1 10
+--   i2 <- map (_ + 1) $ liftEffect $ Random.randomInt 1 10
+--   i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
+--   tell "nothing"
+--   pure $ 4 + i1 + i2 + i3
 
-test5 :: Effect Int
-test5 = map fst test4
+-- test5 :: Effect Int
+-- test5 = map fst test4
