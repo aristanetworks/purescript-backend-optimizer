2911ff60baea058c248a536708819526d6e42116
Jordan Martinez <jordan.martinez@arista.com>
2022-09-16 17:02:35 -0500

    Add directive to StateT program2

--- backend-es/test/snapshots-out/Snapshot.TransformerStateT01.js
+++ backend-es/test/snapshots-out/Snapshot.TransformerStateT01.js
@@ -1,4 +1,5 @@
 // @inline export program1 arity=1
+// @inline export program2 arity=2
 import * as $runtime from "../runtime.js";
 import * as Control$dMonad$dState$dTrans from "../Control.Monad.State.Trans/index.js";
 import * as Data$dSemiring from "../Data.Semiring/index.js";
@@ -10,7 +11,6 @@ import * as Effect$dClass from "../Effect.Class/index.js";
 import * as Effect$dConsole from "../Effect.Console/index.js";
 import * as Effect$dRandom from "../Effect.Random/index.js";
 const monadEffectState = /* #__PURE__ */ Control$dMonad$dState$dTrans.monadEffectState(Effect$dClass.monadEffectEffect);
-const monadStateStateT = /* #__PURE__ */ Control$dMonad$dState$dTrans.monadStateStateT(Effect.monadEffect);
 const test1 = /* #__PURE__ */ (() => {
   const $0 = Control$dMonad$dState$dTrans.monadTransStateT.lift(Effect.monadEffect)(Effect$dConsole.log("foo"))(1);
   return () => {
@@ -49,7 +49,20 @@ const program2 = dictMonadState => {
     }))(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))));
   };
 };
-const test3 = /* #__PURE__ */ program2(monadStateStateT)(monadEffectState)(2);
+const test3 = /* #__PURE__ */ (() => {
+  const Monad0 = monadEffectState.Monad0();
+  const Bind1 = Monad0.Bind1();
+  const Apply0 = Bind1.Apply0();
+  const map1 = Apply0.Functor0().map;
+  const pure1 = Monad0.Applicative0().pure;
+  return Bind1.bind(monadEffectState.liftEffect(Effect$dConsole.log("foo")))(() => Bind1.bind(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10)))(i1 => Bind1.bind(x => () => Data$dTuple.$Tuple(
+    x,
+    x
+  ))(one => Bind1.bind(map1(v => v + one | 0)(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10))))(i2 => Bind1.bind(x => () => Data$dTuple.$Tuple(Data$dUnit.unit, i2))(() => Bind1.bind(Apply0.apply(map1(Data$dSemiring.intAdd)(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10))))(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10))))(i3 => Bind1.bind(x => {
+    const s$p = x + i3 | 0;
+    return () => Data$dTuple.$Tuple(s$p, s$p);
+  })(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))))(2);
+})();
 const program1 = dictMonadEffect => {
   const Monad0 = dictMonadEffect.Monad0();
   const bindStateT1 = Control$dMonad$dState$dTrans.bindStateT(Monad0);
@@ -83,4 +96,4 @@ const test2 = /* #__PURE__ */ (() => {
     return Data$dTuple.$Tuple(s$p, s$p);
   }))(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))))(1);
 })();
-export {monadEffectState, monadStateStateT, program1, program2, test1, test2, test3};
+export {monadEffectState, program1, program2, test1, test2, test3};
