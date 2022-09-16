f1cc20c6fa79fa9addd233bde36cb37a3f5f315b
Jordan Martinez <jordan.martinez@arista.com>
2022-09-16 17:04:43 -0500

    Temporary sanity test: StateT test1 output only

--- backend-es/test/snapshots-out/Snapshot.TransformerStateT01.js
+++ backend-es/test/snapshots-out/Snapshot.TransformerStateT01.js
@@ -2,15 +2,13 @@
 // @inline export program2 arity=2
 import * as $runtime from "../runtime.js";
 import * as Control$dMonad$dState$dTrans from "../Control.Monad.State.Trans/index.js";
-import * as Data$dSemiring from "../Data.Semiring/index.js";
 import * as Data$dShow from "../Data.Show/index.js";
 import * as Data$dTuple from "../Data.Tuple/index.js";
-import * as Data$dUnit from "../Data.Unit/index.js";
 import * as Effect from "../Effect/index.js";
 import * as Effect$dClass from "../Effect.Class/index.js";
 import * as Effect$dConsole from "../Effect.Console/index.js";
 import * as Effect$dRandom from "../Effect.Random/index.js";
-const monadEffectState = /* #__PURE__ */ Control$dMonad$dState$dTrans.monadEffectState(Effect$dClass.monadEffectEffect);
+const liftEffect = /* #__PURE__ */ (() => Control$dMonad$dState$dTrans.monadEffectState(Effect$dClass.monadEffectEffect).liftEffect)();
 const test1 = /* #__PURE__ */ (() => {
   const $0 = Control$dMonad$dState$dTrans.monadTransStateT.lift(Effect.monadEffect)(Effect$dConsole.log("foo"))(1);
   return () => {
@@ -19,8 +17,8 @@ const test1 = /* #__PURE__ */ (() => {
     const a$p = Control$dMonad$dState$dTrans.monadTransStateT.lift(Effect.monadEffect)(Effect$dRandom.randomInt(1)(10))(v1$1._2)();
     const bind = Control$dMonad$dState$dTrans.bindStateT(Effect.monadEffect).bind;
     const pure = Control$dMonad$dState$dTrans.applicativeStateT(Effect.monadEffect).pure;
-    const $6 = monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10));
-    const $7 = monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10));
+    const $6 = liftEffect(Effect$dRandom.randomInt(1)(10));
+    const $7 = liftEffect(Effect$dRandom.randomInt(1)(10));
     const v1$2 = bind(s => {
       const $9 = $6(s);
       return () => {
@@ -32,68 +30,4 @@ const test1 = /* #__PURE__ */ (() => {
     return Data$dTuple.$Tuple(Data$dShow.showIntImpl(v1$1._1 + s$p | 0), s$p);
   };
 })();
-const program2 = dictMonadState => {
-  const get1 = dictMonadState.state(s => Data$dTuple.$Tuple(s, s));
-  return dictMonadEffect => {
-    const Monad0 = dictMonadEffect.Monad0();
-    const Bind1 = Monad0.Bind1();
-    const Apply0 = Bind1.Apply0();
-    const map1 = Apply0.Functor0().map;
-    const pure1 = Monad0.Applicative0().pure;
-    return Bind1.bind(dictMonadEffect.liftEffect(Effect$dConsole.log("foo")))(() => Bind1.bind(dictMonadEffect.liftEffect(Effect$dRandom.randomInt(1)(10)))(i1 => Bind1.bind(get1)(one => Bind1.bind(map1(v => v + one | 0)(dictMonadEffect.liftEffect(Effect$dRandom.randomInt(1)(10))))(i2 => Bind1.bind(dictMonadState.state(v => Data$dTuple.$Tuple(
-      Data$dUnit.unit,
-      i2
-    )))(() => Bind1.bind(Apply0.apply(map1(Data$dSemiring.intAdd)(dictMonadEffect.liftEffect(Effect$dRandom.randomInt(1)(10))))(dictMonadEffect.liftEffect(Effect$dRandom.randomInt(1)(10))))(i3 => Bind1.bind(dictMonadState.state(s => {
-      const s$p = s + i3 | 0;
-      return Data$dTuple.$Tuple(s$p, s$p);
-    }))(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))));
-  };
-};
-const test3 = /* #__PURE__ */ (() => {
-  const Monad0 = monadEffectState.Monad0();
-  const Bind1 = Monad0.Bind1();
-  const Apply0 = Bind1.Apply0();
-  const map1 = Apply0.Functor0().map;
-  const pure1 = Monad0.Applicative0().pure;
-  return Bind1.bind(monadEffectState.liftEffect(Effect$dConsole.log("foo")))(() => Bind1.bind(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10)))(i1 => Bind1.bind(x => () => Data$dTuple.$Tuple(
-    x,
-    x
-  ))(one => Bind1.bind(map1(v => v + one | 0)(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10))))(i2 => Bind1.bind(x => () => Data$dTuple.$Tuple(Data$dUnit.unit, i2))(() => Bind1.bind(Apply0.apply(map1(Data$dSemiring.intAdd)(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10))))(monadEffectState.liftEffect(Effect$dRandom.randomInt(1)(10))))(i3 => Bind1.bind(x => {
-    const s$p = x + i3 | 0;
-    return () => Data$dTuple.$Tuple(s$p, s$p);
-  })(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))))(2);
-})();
-const program1 = dictMonadEffect => {
-  const Monad0 = dictMonadEffect.Monad0();
-  const bindStateT1 = Control$dMonad$dState$dTrans.bindStateT(Monad0);
-  const liftEffect1 = Control$dMonad$dState$dTrans.monadEffectState(dictMonadEffect).liftEffect;
-  const monadStateStateT1 = Control$dMonad$dState$dTrans.monadStateStateT(Monad0);
-  const get1 = monadStateStateT1.state(s => Data$dTuple.$Tuple(s, s));
-  const map1 = Control$dMonad$dState$dTrans.functorStateT(Monad0.Bind1().Apply0().Functor0()).map;
-  const apply1 = Control$dMonad$dState$dTrans.applyStateT(Monad0).apply;
-  const pure1 = Control$dMonad$dState$dTrans.applicativeStateT(Monad0).pure;
-  return bindStateT1.bind(liftEffect1(Effect$dConsole.log("foo")))(() => bindStateT1.bind(liftEffect1(Effect$dRandom.randomInt(1)(10)))(i1 => bindStateT1.bind(get1)(one => bindStateT1.bind(map1(v => v + one | 0)(liftEffect1(Effect$dRandom.randomInt(1)(10))))(i2 => bindStateT1.bind(monadStateStateT1.state(v => Data$dTuple.$Tuple(
-    Data$dUnit.unit,
-    i2
-  )))(() => bindStateT1.bind(apply1(map1(Data$dSemiring.intAdd)(liftEffect1(Effect$dRandom.randomInt(1)(10))))(liftEffect1(Effect$dRandom.randomInt(1)(10))))(i3 => bindStateT1.bind(monadStateStateT1.state(s => {
-    const s$p = s + i3 | 0;
-    return Data$dTuple.$Tuple(s$p, s$p);
-  }))(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))));
-};
-const test2 = /* #__PURE__ */ (() => {
-  const bindStateT1 = Control$dMonad$dState$dTrans.bindStateT(Effect.monadEffect);
-  const liftEffect1 = Control$dMonad$dState$dTrans.monadEffectState(Effect$dClass.monadEffectEffect).liftEffect;
-  const monadStateStateT1 = Control$dMonad$dState$dTrans.monadStateStateT(Effect.monadEffect);
-  const get1 = monadStateStateT1.state(s => Data$dTuple.$Tuple(s, s));
-  const map1 = Control$dMonad$dState$dTrans.functorStateT(Effect.functorEffect).map;
-  const apply1 = Control$dMonad$dState$dTrans.applyStateT(Effect.monadEffect).apply;
-  const pure1 = Control$dMonad$dState$dTrans.applicativeStateT(Effect.monadEffect).pure;
-  return bindStateT1.bind(liftEffect1(Effect$dConsole.log("foo")))(() => bindStateT1.bind(liftEffect1(Effect$dRandom.randomInt(1)(10)))(i1 => bindStateT1.bind(get1)(one => bindStateT1.bind(map1(v => v + one | 0)(liftEffect1(Effect$dRandom.randomInt(1)(10))))(i2 => bindStateT1.bind(monadStateStateT1.state(v => Data$dTuple.$Tuple(
-    Data$dUnit.unit,
-    i2
-  )))(() => bindStateT1.bind(apply1(map1(Data$dSemiring.intAdd)(liftEffect1(Effect$dRandom.randomInt(1)(10))))(liftEffect1(Effect$dRandom.randomInt(1)(10))))(i3 => bindStateT1.bind(monadStateStateT1.state(s => {
-    const s$p = s + i3 | 0;
-    return Data$dTuple.$Tuple(s$p, s$p);
-  }))(result => pure1(Data$dShow.showIntImpl(i1 + result | 0)))))))))(1);
-})();
-export {monadEffectState, program1, program2, test1, test2, test3};
+export {liftEffect, test1};
--- backend-es/test/snapshots/Snapshot.TransformerStateT01.purs
+++ backend-es/test/snapshots/Snapshot.TransformerStateT01.purs
@@ -23,30 +23,30 @@ test1 = 1 # runStateT do
   result <- modify (_ + i3)
   pure $ show $ i1 + result
 
-program1 :: forall m. MonadEffect m => StateT Int m String
-program1 = do
-  liftEffect $ Console.log "foo"
-  i1 <- liftEffect $ Random.randomInt 1 10
-  one <- get
-  i2 <- map (_ + one) $ liftEffect $ Random.randomInt 1 10
-  put i2
-  i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
-  result <- modify (_ + i3)
-  pure $ show $ i1 + result
+-- program1 :: forall m. MonadEffect m => StateT Int m String
+-- program1 = do
+--   liftEffect $ Console.log "foo"
+--   i1 <- liftEffect $ Random.randomInt 1 10
+--   one <- get
+--   i2 <- map (_ + one) $ liftEffect $ Random.randomInt 1 10
+--   put i2
+--   i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
+--   result <- modify (_ + i3)
+--   pure $ show $ i1 + result
 
-test2 :: Effect (Tuple String Int)
-test2 = runStateT program1 1
+-- test2 :: Effect (Tuple String Int)
+-- test2 = runStateT program1 1
 
-program2 :: forall m. MonadState Int m => MonadEffect m => m String
-program2 = do
-  liftEffect $ Console.log "foo"
-  i1 <- liftEffect $ Random.randomInt 1 10
-  one <- get
-  i2 <- map (_ + one) $ liftEffect $ Random.randomInt 1 10
-  put i2
-  i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
-  result <- modify (_ + i3)
-  pure $ show $ i1 + result
+-- program2 :: forall m. MonadState Int m => MonadEffect m => m String
+-- program2 = do
+--   liftEffect $ Console.log "foo"
+--   i1 <- liftEffect $ Random.randomInt 1 10
+--   one <- get
+--   i2 <- map (_ + one) $ liftEffect $ Random.randomInt 1 10
+--   put i2
+--   i3 <- (+) <$> (liftEffect $ Random.randomInt 1 10) <*> (liftEffect $ Random.randomInt 1 10)
+--   result <- modify (_ + i3)
+--   pure $ show $ i1 + result
 
-test3 :: Effect (Tuple String Int)
-test3 = runStateT program2 2
+-- test3 :: Effect (Tuple String Int)
+-- test3 = runStateT program2 2
