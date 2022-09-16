23be5eb6123a6977a3ed2d6c0fc469d49435d9f8
Jordan Martinez <jordan.martinez@arista.com>
2022-09-16 16:16:19 -0500

    Ensure MaybeT snapshot uses map and apply

--- backend-es/test/snapshots-out/Snapshot.TransformerMaybeT01.js
+++ backend-es/test/snapshots-out/Snapshot.TransformerMaybeT01.js
@@ -1,19 +1,32 @@
 import * as $runtime from "../runtime.js";
 import * as Control$dMonad$dMaybe$dTrans from "../Control.Monad.Maybe.Trans/index.js";
 import * as Data$dMaybe from "../Data.Maybe/index.js";
+import * as Data$dSemiring from "../Data.Semiring/index.js";
+import * as Effect from "../Effect/index.js";
 import * as Effect$dClass from "../Effect.Class/index.js";
 import * as Effect$dConsole from "../Effect.Console/index.js";
 import * as Effect$dRandom from "../Effect.Random/index.js";
+const apply = /* #__PURE__ */ (() => Control$dMonad$dMaybe$dTrans.applyMaybeT(Effect.monadEffect).apply)();
 const test1 = /* #__PURE__ */ (() => {
   const $0 = Effect$dConsole.log("foo");
   return () => {
     $0();
     const a$p = Effect$dRandom.randomInt(1)(10)();
     const a$p$1 = Effect$dRandom.randomInt(1)(10)();
-    const $4 = a$p$1 + 1 | 0;
-    const a$p$2 = Effect$dRandom.randomInt(1)(10)();
-    const a$p$3 = Effect$dRandom.randomInt(1)(10)();
-    const v1 = Data$dMaybe.applyMaybe.apply(Data$dMaybe.$Maybe("Just", $7 => a$p$2 + $7 | 0))(Data$dMaybe.$Maybe("Just", a$p$3));
+    const $4 = a$p$1 + 5 | 0;
+    const v1 = apply((() => {
+      const $5 = Effect$dRandom.randomInt(1)(10);
+      return () => {
+        const a$p$2 = $5();
+        return Data$dMaybe.$Maybe("Just", $7 => a$p$2 + $7 | 0);
+      };
+    })())((() => {
+      const $5 = Effect$dRandom.randomInt(1)(10);
+      return () => {
+        const a$p$2 = $5();
+        return Data$dMaybe.$Maybe("Just", a$p$2);
+      };
+    })())();
     if (v1.tag === "Nothing") { return Data$dMaybe.Nothing; }
     if (v1.tag === "Just") { return Data$dMaybe.$Maybe("Just", ((1 + a$p | 0) + $4 | 0) + v1._1 | 0); }
     $runtime.fail();
@@ -22,16 +35,19 @@ const test1 = /* #__PURE__ */ (() => {
 const program1 = dictMonadEffect => {
   const Monad0 = dictMonadEffect.Monad0();
   const bindMaybeT1 = Control$dMonad$dMaybe$dTrans.bindMaybeT(Monad0);
-  const Apply0 = Monad0.Bind1().Apply0();
-  const map2 = Apply0.Functor0().map;
+  const Functor0 = Monad0.Bind1().Apply0().Functor0();
+  const apply1 = Control$dMonad$dMaybe$dTrans.applyMaybeT(Monad0).apply;
   const pure1 = Control$dMonad$dMaybe$dTrans.applicativeMaybeT(Monad0).pure;
-  return bindMaybeT1.bind(map2(Data$dMaybe.Just)(dictMonadEffect.liftEffect(Effect$dConsole.log("foo"))))(() => bindMaybeT1.bind(dictMonadEffect.liftEffect((() => {
+  return bindMaybeT1.bind(Functor0.map(Data$dMaybe.Just)(dictMonadEffect.liftEffect(Effect$dConsole.log("foo"))))(() => bindMaybeT1.bind(dictMonadEffect.liftEffect((() => {
     const $7 = Effect$dRandom.randomInt(1)(10);
     return () => {
       const a$p = $7();
       return Data$dMaybe.$Maybe("Just", a$p);
     };
-  })()))(i1 => bindMaybeT1.bind(map2(v1 => {
+  })()))(i1 => bindMaybeT1.bind(Functor0.map(v1 => {
+    if (v1.tag === "Just") { return Data$dMaybe.$Maybe("Just", v1._1 + 4 | 0); }
+    return Data$dMaybe.Nothing;
+  })(Functor0.map(v1 => {
     if (v1.tag === "Just") { return Data$dMaybe.$Maybe("Just", v1._1 + 1 | 0); }
     return Data$dMaybe.Nothing;
   })(dictMonadEffect.liftEffect((() => {
@@ -40,10 +56,7 @@ const program1 = dictMonadEffect => {
       const a$p = $8();
       return Data$dMaybe.$Maybe("Just", a$p);
     };
-  })())))(i2 => bindMaybeT1.bind(Apply0.apply(map2(a => b => Data$dMaybe.applyMaybe.apply((() => {
-    if (a.tag === "Just") { return Data$dMaybe.$Maybe("Just", $11 => a._1 + $11 | 0); }
-    return Data$dMaybe.Nothing;
-  })())(b))(dictMonadEffect.liftEffect((() => {
+  })()))))(i2 => bindMaybeT1.bind(apply1(Functor0.map(Data$dMaybe.functorMaybe.map(Data$dSemiring.intAdd))(dictMonadEffect.liftEffect((() => {
     const $9 = Effect$dRandom.randomInt(1)(10);
     return () => {
       const a$p = $9();
@@ -58,4 +71,4 @@ const program1 = dictMonadEffect => {
   })())))(i3 => pure1(((1 + i1 | 0) + i2 | 0) + i3 | 0)))));
 };
 const test2 = /* #__PURE__ */ program1(Effect$dClass.monadEffectEffect);
-export {program1, test1, test2};
+export {apply, program1, test1, test2};
--- backend-es/test/snapshots/Snapshot.TransformerMaybeT01.purs
+++ backend-es/test/snapshots/Snapshot.TransformerMaybeT01.purs
@@ -2,7 +2,6 @@ module Snapshot.TransformerMaybeT01 where
 
 import Prelude
 
-import Control.Apply (lift2)
 import Control.Monad.Maybe.Trans (MaybeT(..), runMaybeT)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -14,16 +13,16 @@ test1 :: Effect (Maybe Int)
 test1 = runMaybeT do
   MaybeT $ map Just $ liftEffect $ Console.log "foo"
   i1 <- MaybeT $ liftEffect $ map Just $ Random.randomInt 1 10
-  i2 <- MaybeT $ map (map (_ + 1)) $ liftEffect $ map Just $ Random.randomInt 1 10
-  i3 <- MaybeT $ lift2 (+) <$> (liftEffect $ map Just $ Random.randomInt 1 10) <*> (liftEffect $ map Just $ Random.randomInt 1 10)
+  i2 <- map (_ + 4) $ MaybeT $ map (map (_ + 1)) $ liftEffect $ map Just $ Random.randomInt 1 10
+  i3 <- (+) <$> (MaybeT $ liftEffect $ map Just $ Random.randomInt 1 10) <*> (MaybeT $ liftEffect $ map Just $ Random.randomInt 1 10)
   pure $ 1 + i1 + i2 + i3
 
 program1 :: forall m. MonadEffect m => MaybeT m Int
 program1 = do
   MaybeT $ map Just $ liftEffect $ Console.log "foo"
   i1 <- MaybeT $ liftEffect $ map Just $ Random.randomInt 1 10
-  i2 <- MaybeT $ map (map (_ + 1)) $ liftEffect $ map Just $ Random.randomInt 1 10
-  i3 <- MaybeT $ lift2 (+) <$> (liftEffect $ map Just $ Random.randomInt 1 10) <*> (liftEffect $ map Just $ Random.randomInt 1 10)
+  i2 <- map (_ + 4) $ MaybeT $ map (map (_ + 1)) $ liftEffect $ map Just $ Random.randomInt 1 10
+  i3 <- (+) <$> (MaybeT $ liftEffect $ map Just $ Random.randomInt 1 10) <*> (MaybeT $ liftEffect $ map Just $ Random.randomInt 1 10)
   pure $ 1 + i1 + i2 + i3
 
 test2 :: Effect (Maybe Int)
