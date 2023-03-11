99895666adcd2f558b2d665d9c4742dc6977faa4
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-03-11 17:42:01 -0600

    Correct snapshot

--- backend-es/test/snapshots-out/Snapshot.CaseRecord.js
+++ backend-es/test/snapshots-out/Snapshot.CaseRecord.js
@@ -10,10 +10,12 @@ const test5 = v => {
 };
 const test4 = v => {
   if (v.a > 0) { return v.a; }
+  if (v.b > 1) { return v.b; }
   return 3;
 };
 const test3 = v => {
   if (v.a > 0) { return v.a; }
+  if (v.b > 1) { return v.b; }
   return 3;
 };
 const test2 = v => {
@@ -33,6 +35,12 @@ const test2 = v => {
 };
 const test1 = v => {
   if (v.a === 1) { return "0"; }
+  if (v.b === 1) { return "1"; }
+  if (v.c === 1) { return "2"; }
+  if (v.a === 2) {
+    if (v.b === 2) { return "3"; }
+    return "catch";
+  }
   return "catch";
 };
 export {test1, test2, test3, test4, test5, test6};
