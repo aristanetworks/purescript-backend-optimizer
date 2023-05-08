851e258b48e5c0cee7af413f595b3f286b23fa4e
Nathan Faubion <natefaubion@users.noreply.github.com>
2023-05-08 12:21:12 -0700

    Use shell in test spawn (#51)
    * Use shell in test spawn
    
    * Add attributes for snapshots
    
    * Use file url

--- backend-es/test/Utils.js
+++ backend-es/test/Utils.js
@@ -1,6 +1,13 @@
+import { spawn } from "child_process";
+import { pathToFileURL } from "url";
+
 export const loadModuleMainImpl = onError => onSuccessMain => onSuccess => path => () => {
-  import(path).then(
+  import(pathToFileURL(path)).then(
     mod => mod.main ? onSuccessMain(mod.main)() : onSuccess(),
     err => onError(err)()
   );
+}
+
+export const spawnImpl = cmd => args => () => {
+  return spawn(cmd, args, { shell: true, stdio: "inherit" });
 }
