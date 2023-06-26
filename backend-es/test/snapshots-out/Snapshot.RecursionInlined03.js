a93cd811f6651e4be1a8c19f709df125e21bde4f
Mike Solomon <mike@meeshkan.com>
2023-06-26 15:12:43 +0300

    Adds test that does not work yet for variants

--- backend-es/test/snapshots-out/Snapshot.RecursionInlined03.js
+++ backend-es/test/snapshots-out/Snapshot.RecursionInlined03.js
@@ -1,6 +1,48 @@
-// @inline Snapshot.RecursionInlined03.compose always
-const id = a => a;
-const compose = ab => bc => a => bc(ab(a));
-const test1 = a => a;
-const test2 = z => a => z(a);
-export {compose, id, test1, test2};
+// @inline Snapshot.RecursionInlined03.append always
+// This doesn't quite work yet, because the inlining of append depends
+// on the analysis of the local `b`, which we don't have (easy) access to.
+// If we can somehow grab its usage, we'd likely see that access + case == total
+import * as Partial from "../Partial/index.js";
+const List = x => x;
+const nil = {type: "nil", value: undefined};
+const cons = head => tail => ({type: "cons", value: {head, tail}});
+const append = v => b => {
+  if (v.type === "cons") { return {type: "cons", value: {head: v.value.head, tail: append(v.value.tail)(b)}}; }
+  if (v.type === "nil") { return b; }
+  return Partial._crashWith("Data.Variant: pattern match failure [" + v.type + "]");
+};
+const test1 = {
+  type: "cons",
+  value: {
+    head: "a",
+    tail: {
+      type: "cons",
+      value: {
+        head: "b",
+        tail: /* #__PURE__ */ append({type: "cons", value: {head: "c", tail: nil}})({
+          type: "cons",
+          value: {head: "d", tail: {type: "cons", value: {head: "e", tail: {type: "cons", value: {head: "f", tail: {type: "cons", value: {head: "g", tail: nil}}}}}}}
+        })
+      }
+    }
+  }
+};
+const test2 = z => (
+  {
+    type: "cons",
+    value: {
+      head: "a",
+      tail: {
+        type: "cons",
+        value: {
+          head: "b",
+          tail: append({type: "cons", value: {head: "c", tail: z}})({
+            type: "cons",
+            value: {head: "d", tail: {type: "cons", value: {head: "e", tail: {type: "cons", value: {head: "f", tail: {type: "cons", value: {head: "g", tail: nil}}}}}}}
+          })
+        }
+      }
+    }
+  }
+);
+export {List, append, cons, nil, test1, test2};
--- backend-es/test/snapshots/Snapshot.RecursionInlined03.purs
+++ backend-es/test/snapshots/Snapshot.RecursionInlined03.purs
@@ -1,13 +1,31 @@
--- @inline Snapshot.RecursionInlined03.compose always
+-- @inline Snapshot.RecursionInlined03.append always
+-- This doesn't quite work yet, because the inlining of append depends
+-- on the analysis of the local `b`, which we don't have (easy) access to.
+-- If we can somehow grab its usage, we'd likely see that access + case == total
 module Snapshot.RecursionInlined03 where
 
-compose :: forall a b c. (a -> b) -> (b -> c) -> (a -> c)
-compose ab bc a = bc (ab a)
+import Prelude
 
-id :: forall a. a -> a
-id a = a
+import Data.Variant (Variant, inj, case_, on)
+import Type.Proxy (Proxy(..))
 
-test1 :: Int -> Int
-test1 =compose id (compose id (compose id (compose id id)))
-test2 :: (Int -> Int) -> Int -> Int
-test2 z = compose id (compose id (compose z (compose id id)))
+newtype List a = List (Variant (nil :: Unit, cons :: { head :: a, tail :: List a }))
+
+cons :: forall a. a -> List a -> List a
+cons head tail = List $ inj (Proxy :: _ "cons") {head, tail}
+nil :: forall a. List a
+nil = List $ inj (Proxy :: _ "nil") unit
+infixr 5 cons as :
+
+append :: forall a. List a -> List a -> List a
+append (List a) b = (case_
+  # on (Proxy :: _ "nil") (\_ -> b)
+  # on (Proxy :: _ "cons") (\{head, tail} -> cons head (append tail b))) a
+
+infixr 4 append as <>
+
+test1 :: List String
+test1 = ("a" : "b" : "c" : nil) <> ("d" : "e" : "f" : "g" : nil)
+
+test2 :: List String -> List String
+test2 z = ("a" : "b" : "c" : z) <> ("d" : "e" : "f" : "g" : nil)
