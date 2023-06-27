763838fd99d4cab41bb04e8e90b389e38f0749ec
Mike Solomon <mike@meeshkan.com>
2023-06-27 22:20:22 +0300

    Adds second pass for brokenness

--- backend-es/test/snapshots-out/Snapshot.RecursionInlined02.js
+++ backend-es/test/snapshots-out/Snapshot.RecursionInlined02.js
@@ -7,6 +7,6 @@ const addStuff = x => ys => {
   if (x < 0) { return -1 + addStuff(x + 1 | 0)(ys) | 0; }
   return ys;
 };
-const test1 = /* #__PURE__ */ addStuff(38)(4);
-const test2 = z => addStuff(3)(addStuff(z)(5));
+const test1 = /* #__PURE__ */ (() => 38 + 4 | 0)();
+const test2 = z => 3 + addStuff(z)(5) | 0;
 export {$List, Cons, Nil, addStuff, test1, test2};
