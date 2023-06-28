40ccebb9841926df72bea2300e57a8338ea81107
Mike Solomon <mike@meeshkan.com>
2023-06-28 11:39:40 +0300

    Adds foldlArray

---
+++ backend-es/test/snapshots-out/Snapshot.RecursionInlined04.js
@@ -0,0 +1,33 @@
+// @inline Snapshot.RecursionInlined04.foldlArray always
+// @inline Snapshot.RecursionInlined04.foldlArray2 always
+// A "real-world" example from deku
+import * as Data$dSemigroup from "../Data.Semigroup/index.js";
+const foldlArray2 = foldlArray2$a0$copy => foldlArray2$a1$copy => foldlArray2$a2$copy => foldlArray2$a3$copy => foldlArray2$a4$copy => {
+  let foldlArray2$a0 = foldlArray2$a0$copy;
+  let foldlArray2$a1 = foldlArray2$a1$copy;
+  let foldlArray2$a2 = foldlArray2$a2$copy;
+  let foldlArray2$a3 = foldlArray2$a3$copy;
+  let foldlArray2$a4 = foldlArray2$a4$copy;
+  let foldlArray2$c = true;
+  let foldlArray2$r;
+  while (foldlArray2$c) {
+    const n = foldlArray2$a0, i = foldlArray2$a1, bab = foldlArray2$a2, b = foldlArray2$a3, arr = foldlArray2$a4;
+    if (n === (i - 1 | 0)) {
+      foldlArray2$c = false;
+      foldlArray2$r = b;
+      continue;
+    }
+    foldlArray2$a0 = n + 1 | 0;
+    foldlArray2$a1 = i;
+    foldlArray2$a2 = bab;
+    foldlArray2$a3 = bab(b)(arr[n]);
+    foldlArray2$a4 = arr;
+  }
+  return foldlArray2$r;
+};
+const foldlArray = bab => b => arr => foldlArray2(0)(arr.length)(bab)(b)(arr);
+const test1 = /* #__PURE__ */ (() => {
+  const $0 = ["a", "b", "c"];
+  return foldlArray2(0)($0.length)(Data$dSemigroup.concatString)("")($0)(Data$dSemigroup.concatString)("")(["a", "b", "c"]);
+})();
+export {foldlArray, foldlArray2, test1};
