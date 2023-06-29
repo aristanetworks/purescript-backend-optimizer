a6e35467f66c28e4a5487742c9d45d3393727b13
Mike Solomon <mike@meeshkan.com>
2023-06-29 20:17:13 +0300

    Updates test

---
+++ backend-es/test/snapshots/Snapshot.InlineLocalReferenceOpArrayLength.purs
@@ -0,0 +1,13 @@
+-- @inline Snapshot.PrimOnLocals.a always
+
+module Snapshot.InlineLocalReferenceOpArrayLength where
+
+import Prelude
+
+import Data.Array as Array
+
+shouldInlineArrayLength :: Array Int -> Array Int
+shouldInlineArrayLength = Array.snoc <*> Array.length
+
+b :: Array Int
+b = shouldInlineArrayLength [ 1, 2, 3 ]
