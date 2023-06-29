d8a97e30604aae6420fe610e0770ec4cff4e63bd
Mike Solomon <mike@meeshkan.com>
2023-06-29 22:27:07 +0300

    Revert "Adds new test"
    This reverts commit b10fe3058ff34469222c2680eefc9d44273e7d95.

--- backend-es/test/snapshots-out/Snapshot.InlineLocalReferenceOpArrayIndex.js
+++
@@ -1,4 +0,0 @@
-import * as Data$dTuple from "../Data.Tuple/index.js";
-const shouldInlineArrayIndex = () => x => Data$dTuple.$Tuple(x[0], x[1]);
-const test = /* #__PURE__ */ Data$dTuple.$Tuple(44, 48);
-export {shouldInlineArrayIndex, test};
