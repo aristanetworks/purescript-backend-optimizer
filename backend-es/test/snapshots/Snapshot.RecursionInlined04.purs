cf72cd8aea64c041e37c23d8158f4913e98d86fe
Mike Solomon <mike@meeshkan.com>
2023-06-30 07:41:04 +0300

    Changes test 4 to something from deku

--- backend-es/test/snapshots/Snapshot.RecursionInlined04.purs
+++ backend-es/test/snapshots/Snapshot.RecursionInlined04.purs
@@ -7,17 +7,24 @@ module Snapshot.RecursionInlined04 where
 import Prelude
 
 import Data.Array as Array
+import Data.Either (Either(..))
 import Partial.Unsafe (unsafePartial)
 
-foldlArray :: forall a b . (b -> a -> b) -> b -> Array a -> b
+foldlArray :: forall a b. (b -> a -> b) -> b -> Array a -> b
 foldlArray bab b arr = foldlArray2 0 len bab b arr
   where
   len = Array.length arr
 
-foldlArray2 :: forall a b . Int -> Int -> (b -> a -> b) -> b -> Array a -> b
+foldlArray2 :: forall a b. Int -> Int -> (b -> a -> b) -> b -> Array a -> b
 foldlArray2 n i bab b arr
   | n == i = b
   | otherwise = foldlArray2 (n + 1) i bab (bab b (unsafePartial $ Array.unsafeIndex arr n)) arr
 
-test1 :: String
-test1 = foldlArray (<>) "" ["a", "b", "c"]
+test1 :: { left :: Array String, right :: Array String }
+test1 = foldlArray
+  ( \{ left, right } -> case _ of
+      Left x -> { left: left <> [ x ], right }
+      Right x -> { left, right: right <> [ x ] }
+  )
+  { left: [], right: [] }
+  [ Left "a" ]
