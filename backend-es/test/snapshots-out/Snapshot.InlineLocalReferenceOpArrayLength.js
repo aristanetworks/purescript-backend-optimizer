6f2f6f94679a303ff72857341d1c791a188798a9
Nathan Faubion <nathan@n-son.com>
2023-07-02 11:16:18 -0700

    Commit snapshot result

--- backend-es/test/snapshots-out/Snapshot.InlineLocalReferenceOpArrayLength.js
+++ backend-es/test/snapshots-out/Snapshot.InlineLocalReferenceOpArrayLength.js
@@ -1,4 +1,2 @@
-import * as Data$dArray from "../Data.Array/index.js";
-const shouldInlineArrayLength = x => Data$dArray.snoc(x)(x.length);
-const test = /* #__PURE__ */ Data$dArray.snoc([1, 2, 3])(3);
-export {shouldInlineArrayLength, test};
+const test = fn => [];
+export {test};
