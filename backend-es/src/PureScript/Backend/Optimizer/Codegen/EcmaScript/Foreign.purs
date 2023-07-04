50dbd61771420e15f8a2f69ad44514b862aea87e
Nathan Faubion <nathan@n-son.com>
2023-07-03 18:07:11 -0700

    Move index inline to ES foreign, add assertion test for bounds check

--- backend-es/src/PureScript/Backend/Optimizer/Codegen/EcmaScript/Foreign.purs
+++ backend-es/src/PureScript/Backend/Optimizer/Codegen/EcmaScript/Foreign.purs
@@ -4,14 +4,15 @@ import Prelude
 
 import Data.Array.NonEmpty as NonEmptyArray
 import Data.Enum (fromEnum)
+import Data.Lazy as Lazy
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import PureScript.Backend.Optimizer.CoreFn (Ident, Literal(..), Qualified)
-import PureScript.Backend.Optimizer.Semantics (BackendSemantics(..), ExternSpine(..), evalApp, evalPrimOp, makeLet)
+import PureScript.Backend.Optimizer.Semantics (BackendSemantics(..), ExternSpine(..), SemConditional(..), evalApp, evalPrimOp, liftInt, makeLet)
 import PureScript.Backend.Optimizer.Semantics.Foreign (ForeignEval, ForeignSemantics, qualified)
-import PureScript.Backend.Optimizer.Syntax (BackendOperator(..), BackendOperator2(..))
+import PureScript.Backend.Optimizer.Syntax (BackendOperator(..), BackendOperator1(..), BackendOperator2(..), BackendOperatorOrd(..))
 import PureScript.Backend.Optimizer.Utils (foldr1Array)
 
 esForeignSemantics :: Map (Qualified Ident) ForeignEval
@@ -19,6 +20,7 @@ esForeignSemantics = Map.fromFoldable
   [ control_monad_st_internal_for
   , control_monad_st_internal_foreach
   , control_monad_st_internal_while
+  , data_array_indexImpl
   , data_array_st_push
   , data_array_st_unshift
   , data_array_st_unsafeFreeze
@@ -43,6 +45,29 @@ esForeignSemantics = Map.fromFoldable
   , foreign_object_st_poke
   , foreign_object_st_unsafe_unsafeFreeze
   ]
+
+data_array_indexImpl :: ForeignSemantics
+data_array_indexImpl = Tuple (qualified "Data.Array" "indexImpl") go
+  where
+  go env _ = case _ of
+    [ ExternUncurriedApp [ just, nothing, arr, ix ] ] ->
+      Just $ makeLet Nothing arr \arr' ->
+        makeLet Nothing ix \ix' ->
+          SemBranch
+            ( NonEmptyArray.singleton $ SemConditional
+                ( Lazy.defer \_ ->
+                    evalPrimOp env $ Op2 OpBooleanAnd
+                      (evalPrimOp env (Op2 (OpIntOrd OpGte) ix' (liftInt 0)))
+                      (evalPrimOp env (Op2 (OpIntOrd OpLt) ix' (evalPrimOp env (Op1 OpArrayLength arr'))))
+                )
+                ( Lazy.defer \_ ->
+                    evalApp env just
+                      [ evalPrimOp env (Op2 OpArrayIndex arr' ix') ]
+                )
+            )
+            (pure nothing)
+    _ ->
+      Nothing
 
 data_array_st_push :: ForeignSemantics
 data_array_st_push = Tuple (qualified "Data.Array.ST" "push") $ arraySTAll (qualified "Data.Array.ST" "pushAll")
