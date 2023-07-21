01dd701044fa5a389e4c1eb87f830a80a0b25d8a
Mike Solomon <mike@meeshkan.com>
2023-07-21 13:15:25 +0300

    Fixes formatting

--- postinstall/src/Main.purs
+++ postinstall/src/Main.purs
@@ -13,12 +13,12 @@ foreign import rmRecursive :: String -> Effect Unit
 main :: Effect Unit
 main = do
   pure unit
-  -- initCwd <- Object.lookup "INIT_CWD" <$> Process.getEnv
-  -- for_ initCwd \cwd -> do
-  --   let filePath = Path.concat [ cwd, "purs-backend-es-processors" ]
-  --   backendProcessorsExists <- FS.exists filePath
-  --   when backendProcessorsExists do
-  --     let externalPath = Path.concat [ cwd, "node_modules", "purs-backend-es", "external" ]
-  --     rmRecursive externalPath
-  --     copyFolderSync filePath externalPath
-  --     runBuild cwd
+-- initCwd <- Object.lookup "INIT_CWD" <$> Process.getEnv
+-- for_ initCwd \cwd -> do
+--   let filePath = Path.concat [ cwd, "purs-backend-es-processors" ]
+--   backendProcessorsExists <- FS.exists filePath
+--   when backendProcessorsExists do
+--     let externalPath = Path.concat [ cwd, "node_modules", "purs-backend-es", "external" ]
+--     rmRecursive externalPath
+--     copyFolderSync filePath externalPath
+--     runBuild cwd
